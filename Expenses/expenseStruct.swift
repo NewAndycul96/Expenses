2a8c25bf0ba5c8c165428f89fa3f4d0abb45cba6
Anand Kulkarni <anandkulkarni@Anands-MacBook-Pro.local>
2018-08-23 14:55:54 -0500

    First commit

---
+++ Expenses/expenseStruct.swift
@@ -0,0 +1,15 @@
+//
+//  expenseStruct.swift
+//  Expenses
+//
+//  Created by Anand Kulkarni on 8/23/18.
+//  Copyright © 2018 Anand Kulkarni. All rights reserved.
+//
+
+import UIKit
+
+struct expense {
+    var title: String
+    var amount: Double
+    var date: Date
+}
